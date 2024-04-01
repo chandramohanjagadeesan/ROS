fcbbeef384acea882094054d4a40d2237e89ba5f
chandramohanjagadeesan <chandramohanj1095@gmail.com>
2024-04-01 20:59:58 +0200

    Added a node for drawing a circle and publishing it

---
+++ ros2_ws/src/my_robot_controller/my_robot_controller/draw_circle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class DrawCircleNode(Node):
+
+    def __init__(self):
+        super().__init__("draw_circle")
+        self.cmd_vel_pub = self.create_publisher(Twist, "/turtle1/cmd_vel",10) #initialize a publisher
+        self.get_logger().info("Draw circle is started")
+        self.timer = self.create_timer(0.5, self.send_velocity_cmd)
+
+    def send_velocity_cmd(self):
+        msg = Twist()
+        msg.linear.x = 2.0
+        msg.angular.z = 1.0
+        self.cmd_vel_pub.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = DrawCircleNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
