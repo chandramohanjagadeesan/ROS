bcec544349067c2ef65993f28a48035459586312
chandramohanjagadeesan <chandramohanj1095@gmail.com>
2024-04-02 12:43:12 +0200

    used a ros2 service

--- ros2_ws/src/my_robot_controller/my_robot_controller/turtle_controller.py
+++ ros2_ws/src/my_robot_controller/my_robot_controller/turtle_controller.py
@@ -3,11 +3,14 @@ import rclpy
 from rclpy.node import Node
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
+from turtlesim.srv import SetPen
+from functools import partial
 
 class TurtleControllerNode(Node):
 
     def __init__(self):
         super().__init__("turtle_controller")
+        self.previous_x = 0
         self.get_logger().info("Turtle Controller is Started")
         self.cmd_vel_publisher = self.create_publisher(
             Twist, "/turtle1/cmd_vel", 10)
@@ -24,7 +27,36 @@ class TurtleControllerNode(Node):
             cmd.angular.z = 0.0
         self.cmd_vel_publisher.publish(cmd)
 
+        if pose.x > 5.5 and self.previous_x <=5.5: # Services needs to be called only when needed
+            self.get_logger().info("Setpen color Red!")
+            self.previous_x = pose.x
+            self.call_set_pen_service(255, 0, 0, 3, 0)
+        elif pose.x <= 5.5 and self.previous_x > 5.5:
+            self.get_logger().info("Setpen color Green!")
+            self.previous_x = pose.x
+            self.call_set_pen_service(0, 255, 0, 3, 0)
 
+    def call_set_pen_service(self, r, g, b, width, off):
+        client = self.create_client(
+            SetPen, "/turtle1/set_pen") #create a cleint service 
+        while not client.wait_for_service(1.0):
+            self.get_logger().warn("Waiting for service") #wait for the service
+        
+        request = SetPen.Request() #create a request
+        request.r = r
+        request.g = g
+        request.b = b
+        request.width = width
+        request.off = off
+
+        future = client.call_async(request) #send the request to the serer gives a future object
+        future.add_done_callback(partial(self.callback_set_pen)) #adding callback when the service replies
+
+    def callback_set_pen(self, future):
+        try:
+            response = future.result()
+        except Exception as e:
+            self.get_logger().error("service call failed: %r" % (e,))  #add exceptions if callback does not work
 
 def main(args=None):
     rclpy.init(args=args)
